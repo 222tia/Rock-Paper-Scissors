31ffe02c0ccda2f2151d6401ce13debf7ccea969
Tia Self <tiaself2002@gmail.com>
2023-03-21 19:21:58 -0400

    Add tests for addComputerScore

--- src/test/java/edu/bsu/cs22/TestScoreKeeper.java
+++ src/test/java/edu/bsu/cs22/TestScoreKeeper.java
@@ -7,50 +7,98 @@ import org.junit.jupiter.api.Test;
 public class TestScoreKeeper {
 
     @Test
-    public void testScoreGainFrom0() {
+    public void testUserScoreGainFrom0() {
         final String userPlay = "paper";
         final String computerPlay = "rock";
         final int userScore = 0;
-        Assertions.assertEquals(1, ScoreKeeper.addScore(computerPlay, userPlay, userScore));
+        Assertions.assertEquals(1, ScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
     }
 
     @Test
-    public void testScoreGainFrom1() {
+    public void testUserScoreGainFrom1() {
         final String userPlay = "scissors";
         final String computerPlay = "paper";
         final int userScore = 1;
-        Assertions.assertEquals(2, ScoreKeeper.addScore(computerPlay, userPlay, userScore));
+        Assertions.assertEquals(2, ScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
     }
 
     @Test
-    public void testScoreGainFrom2() {
+    public void testUserScoreGainFrom2() {
         final String userPlay = "rock";
         final String computerPlay = "scissors";
         final int userScore = 2;
-        Assertions.assertEquals(3, ScoreKeeper.addScore(computerPlay, userPlay, userScore));
+        Assertions.assertEquals(3, ScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
     }
 
     @Test
-    public void testNoScoreGainUserPlaysRock() {
+    public void testNoUserScoreRock() {
         final String userPlay = "rock";
         final String computerPlay = "paper";
         final int userScore = 0;
-        Assertions.assertEquals(0, ScoreKeeper.addScore(computerPlay, userPlay, userScore));
+        Assertions.assertEquals(0, ScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
     }
 
     @Test
-    public void testNoScoreGainUserPlaysPaper() {
+    public void testNoUserScoreGainPaper() {
         final String userPlay = "paper";
         final String computerPlay = "scissors";
         final int userScore = 0;
-        Assertions.assertEquals(0, ScoreKeeper.addScore(computerPlay, userPlay, userScore));
+        Assertions.assertEquals(0, ScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
     }
 
     @Test
-    public void testNoScoreGainUserPlaysScissors() {
+    public void testNoUserScoreGainScissors() {
         final String userPlay = "scissors";
         final String computerPlay = "rock";
         final int userScore = 0;
-        Assertions.assertEquals(0, ScoreKeeper.addScore(computerPlay, userPlay, userScore));
+        Assertions.assertEquals(0, ScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testComputerScoreGainFrom0() {
+        final String userPlay = "rock";
+        final String computerPlay = "paper";
+        final int computerScore = 0;
+        Assertions.assertEquals(1, ScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreGainFrom1() {
+        final String userPlay = "paper";
+        final String computerPlay = "scissors";
+        final int computerScore = 1;
+        Assertions.assertEquals(2, ScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreGainFrom2() {
+        final String userPlay = "scissors";
+        final String computerPlay = "rock";
+        final int computerScore = 2;
+        Assertions.assertEquals(3, ScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreRock() {
+        final String userPlay = "paper";
+        final String computerPlay = "rock";
+        final int computerScore = 0;
+        Assertions.assertEquals(0, ScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreGainPaper() {
+        final String userPlay = "scissors";
+        final String computerPlay = "paper";
+        final int computerScore = 0;
+        Assertions.assertEquals(0, ScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testNoComputerScoreGainScissors() {
+        final String userPlay = "rock";
+        final String computerPlay = "scissors";
+        final int computerScore = 0;
+        Assertions.assertEquals(0, ScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
     }
 }
